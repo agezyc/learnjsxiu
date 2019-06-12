395d192968f087543697425b99f9f83875bf533b
agezyc <29426162@qq.com>
2019-06-12 13:17:58 +0800

    update stop

---
+++ js1/t1.js
@@ -0,0 +1,78 @@
+var node = document.getElementsByClassName('box');
+var arr = [0,1,2,3,4,5,6,7,8]
+var result = [];
+var total =3;
+var flag;
+var colors = [];
+var dq = null;
+
+function getRandomColor( ) {
+    var rand = Math.floor(Math.random( ) * 0xFFFFFF).toString(16);
+    if(rand.length == 6){
+        return rand;
+    }else{
+        return getRandomColor();
+    }
+}
+
+
+function s(){
+        
+        for(var i=0;i<9;i++){
+            node[i].style.background = '#ffa500';
+            
+        }
+
+        for(var i=0; i< total; i++){
+            var ran = Math.floor(Math.random() * (arr.length - i));
+            result.push(arr[ran]);
+            arr[ran] = arr[arr.length-i-1];
+        }
+
+        // while (colors.length < 3){
+        //     var color = "#";
+        //     for (var i = 0; i < 3; i++) {
+        //         var n = Math.round((Math.random() * 255));
+        //         console.log(n);
+        //         if (n <= 16) {
+        //             color += "0";
+        //             color += n.toString(16);
+        //         } else {
+        //             color += Math.round((Math.random() * 255)).toString(16);
+        //         }
+        //     }
+        //     colors.push(color);
+        // }
+
+        while(colors.length<3){
+            var f = "#" + getRandomColor()
+            colors.push(f);
+        }
+       
+    
+    // body
+        console.log(result);
+        console.log(colors);
+        node[result[0]].style.background = colors[0];
+        console.log(result[0] + colors[0]);
+        node[result[1]].style.background = colors[1];
+        node[result[2]].style.background = colors[2];
+        colors =[];
+        arr = [0,1,2,3,4,5,6,7,8];
+        result=[];
+    }
+
+    
+   
+function start(){
+    dq = setInterval(s,1000);
+}
+
+function stop (arguments) {
+    // body
+    for(var i=0;i<9;i++){
+        node[i].style.background = '#ffa500';
+        
+    }
+   clearInterval(dq);  
+}
